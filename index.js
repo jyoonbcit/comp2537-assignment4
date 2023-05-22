a33fe7d3879afa5ea36bb7242f0a0be27d064048
Jihoon Yoon <jihoonyoon0@gmail.com>
2023-05-22 16:08:03 -0700

    Fix third card selection bugs and implement 8

---
+++ index.js
@@ -0,0 +1,41 @@
+const setup = () => {
+    let firstCard = undefined
+    let secondCard = undefined
+    $(".card").on(("click"), function () {
+        $(this).toggleClass("flip");
+
+        if (!firstCard) {
+            // Select and set first card
+            firstCard = $(this).find(".front_face")[0]
+        } else if (!secondCard) {
+            // Select and set second card
+            secondCard = $(this).find(".front_face")[0]
+            console.log(firstCard, secondCard);
+            if ((firstCard.src == secondCard.src) && (firstCard.id != secondCard.id)) {
+                // Different but matching card selected
+                console.log("match")
+                $(`#${firstCard.id}`).parent().off("click")
+                $(`#${secondCard.id}`).parent().off("click")
+            } else if (firstCard.src != secondCard.src) {
+                // Different card selected
+                console.log("no match")
+                setTimeout(() => {
+                    $(`#${firstCard.id}`).parent().toggleClass("flip")
+                    $(`#${secondCard.id}`).parent().toggleClass("flip")
+                }, 1000)
+            } else {
+                // If same card is selected
+                console.log("Can't click the same card twice!")
+                $(this).toggleClass("flip")
+                secondCard = undefined
+            }
+        } else {
+            // If first and second card are set
+            firstCard = $(this).find(".front_face")[0];
+            secondCard = undefined;
+            console.log("New first card:", firstCard);
+        }
+    });
+}
+
+$(document).ready(setup)
